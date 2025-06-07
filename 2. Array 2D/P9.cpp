d00e10ebc975dbe967d27e42d51c9431e7d33742
GIT-RAUNAK <raunak2808.business@gmail.com>
2025-06-07 08:27:52 +0530

    Spiral Order II

--- 2. Array 2D/P9.cpp
+++ 2. Array 2D/P9.cpp
@@ -1,4 +1,4 @@
-/*Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.*/
+//Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.*
 
 #include<iostream>
 #include<vector>
